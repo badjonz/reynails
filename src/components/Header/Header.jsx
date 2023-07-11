849addf271006da7818419865eca001f2948db67
Jon Kumar <badjonz@gmail.com>
2023-07-11 22:03:20 +0900

    Almost finished the nav

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,40 @@
 import React from 'react';
+import { FaInstagram, FaFacebookF, FaTwitter } from 'react-icons/fa';
+import { BsFillEnvelopeAtFill } from 'react-icons/bs';
 
 const Header = () => {
   return (
     <nav className='nav'>
       <div className='nav-container'>
         <div className='nav-row--1'>
-          <p>reyleciadavidson@gmail.com</p>
+          <a className='nav-contact__link' href='#'>
+            <span className='nav-contact__icon'>
+              <BsFillEnvelopeAtFill size={12} />
+            </span>
+            <span className='nav-contact__email'>
+              reyleciadavidson@gmail.com
+            </span>
+          </a>
           <div className='nav-socials'>
-            <a href='#'>Insta</a>
-            <a href='#'>Twitt</a>
-            <a href='#'>Face</a>
+            <div className='nav-socials__item'>
+              <a className='nav-socials__link' href='#'>
+                <FaInstagram size={14} />
+              </a>
+            </div>
+            <div className='nav-socials__item'>
+              <a className='nav-socials__link' href='#'>
+                <FaTwitter size={14} />
+              </a>
+            </div>
+            <div className='nav-socials__item'>
+              <a className='nav-socials__link' href='#'>
+                <FaFacebookF size={14} />
+              </a>
+            </div>
           </div>
         </div>
         <div className='nav-row--2'>
-          <h1>Rey Nails</h1>
+          <h1 className='nav-logo'>Raze Nails</h1>
           <ul className='nav-menu__list'>
             <li className='nav-menu__item'>
               <a href='#' className='nav-menu__link'>
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+      <header className='header'>
+        <div className='header-container'>
+          <h1>
+            <span className='header-main'>
+              Enjoy the Best Nail Designs at Raze Nails
+            </span>
+
+            <span className='header-secondary'>
+              Make your nails perfect right now!
+            </span>
+          </h1>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Hero;
